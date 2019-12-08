64df861df056bc9742358295362cc94ca243788b
TrigonaMinima <shivamrana95@yahoo.in>
2019-12-08 23:58:17 +0530

    Decorator to restrict the commands and other bot features to the legit users

---
+++ bot/utils/decs.py
@@ -0,0 +1,23 @@
+from functools import wraps
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from utils.fileio import load_users
+
+
+users = load_users()
+
+
+def restricted(func):
+    """
+    Decorator to only allow the specific users to call the functions.
+    """
+    @wraps(func)
+    def wrapped(update: Update, context: CallbackContext, *args, **kwargs):
+        user_id = str(update.effective_user.id)
+        if users and user_id not in users:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
+
+        return func(update, context, *args, **kwargs)
+    return wrapped
