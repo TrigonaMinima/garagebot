fe6ccc0675c39aaf67559666a5bda8e11afdf4bd
TrigonaMinima <shivamrana95@yahoo.in>
2019-12-10 21:52:37 +0530

    Setting up hard_replies file

--- bot/bot_setup.py
+++ bot/bot_setup.py
@@ -2,11 +2,30 @@ from pathlib import Path
 
 from utils import fileio
 from utils.db import generic
-from utils.generic import load_config
+from utils.fileio import config
 
+
+hard_repl = {
+    "start": {
+        "default": "Hi!"
+    },
+    "fetch": {
+        "default": "Provide the right argument bro!"
+    },
+    "add_key_val": {
+        "default_y": "Added to the list!",
+        "default_n": "Provide the right argument bro!"
+    },
+    "pop_key_val": {
+        "default_y": "Removed from the list!",
+        "default_n": "Nothing to remove!"
+    },
+    "random_highlight": {
+        "default": "Nothing to show!"
+    }
+}
 
 if __name__ == "__main__":
-    config = load_config()
     assets_dir = Path(config["DIR"]["assets"])
 
     db_path = assets_dir / config["DB"]["file"]
@@ -19,27 +38,35 @@ if __name__ == "__main__":
 
     users_f = assets_dir / config["FILES"]["users_f"]
     fileio.create_file(users_f)
-    msg = ("> Empty users file created. "
-           "This means everone (even strangers) will "
+    msg = (f"> Empty users file ({users_f}) created. "
+           "This means everyone (even strangers) will "
            "be allowed to use the commands")
     print(msg)
 
     start_f = assets_dir / config["FILES"]["start_f"]
     fileio.create_file(start_f)
-    msg = ("> Empty starters file created. "
+    msg = (f"> Empty starters file ({start_f}) created. "
            "Add more starter lines for the bot to use.")
     print(msg)
 
     key_val_f = assets_dir / config["FILES"]["key_val_f"]
     fileio.create_file(key_val_f)
-    msg = ("> Empty key-value pair JSON created. "
+    msg = (f"> Empty key-value pair JSON ({key_val_f}) created. "
            "Add some key-value pairs you want the "
            "bot to fetch when you want it.")
     print(msg)
 
     highlight_f = assets_dir / config["FILES"]["highlight_f"]
     fileio.create_file(highlight_f)
-    msg = ("> Empty highlights file created. "
-           "Add some awesome dialogues or sayings or group memories for"
-           "the bot to give them to you at random.")
+    msg = (f"> Empty highlights file ({highlight_f}) created. "
+           "Add some awesome dialogues or sayings or group memories for "
+           "the bot.")
+    print(msg)
+
+    hard_repl_f = assets_dir / config["FILES"]["hard_repl_f"]
+    fileio.create_file(hard_repl_f)
+    if hard_repl_f.stat().st_size == 0:
+        fileio.dump_json(hard_repl_f, hard_repl)
+    msg = (f"> Default hard replies file ({hard_repl_f}) created. "
+           "Update the default replies for each command as needed")
     print(msg)
